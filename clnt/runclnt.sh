1b9bdf1b614e84e4c117e61ee67a4b4338582f44
annabellerondeau <annabellehrondeau@gmail.com>
2025-11-21 15:55:10 -0500

    Initial commit of starting code

---
+++ clnt/runclnt.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ -z "$ZOOBINDIR" ]]
+then
+    echo "Error!! ZOOBINDIR is not set" 1>&2
+    exit 1
+fi
+
+. $ZOOBINDIR/zkEnv.sh
+
+# TODO Include your ZooKeeper connection string here. Make sure there are no spaces.
+# 	Replace with your server names and client ports.
+export ZKSERVER=tr-open-XX.cs.mcgill.ca:218XX,tr-open-XX.cs.mcgill.ca:218XX,tr-open-XX.cs.mcgill.ca:218XX
+# export ZKSERVER=open-gpu-XX.cs.mcgill.ca:218XX,open-gpu-XX.cs.mcgill.ca:218XX,open-gpu-XX.cs.mcgill.ca:218XX
+
+java -cp $CLASSPATH:../task:.: DistClient "$@"
